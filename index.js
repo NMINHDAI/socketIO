c645a69a0eb81d8eb67cb3f7d3626821a36c0392
NMINHDAI <ngominhdai92tn@gmail.com>
2022-03-21 23:34:11 +0700

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(process.env.PORT || 8900, {
   cors: {
-    origin: "https://cors-proxy420.herokuapp.com/https://brave-spence-43c8e2.netlify.app",
+    origin: "https://brave-spence-43c8e2.netlify.app",
   },
 });
 
